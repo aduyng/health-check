7bcd73bc6536beea87c45707f3d7682a514c1554
Duy Nguyen <aduyng@gmail.com>
2015-07-10 16:58:48 -0500

    config changes

--- config.js
+++ config.js
@@ -1,36 +1,38 @@
 var path = require('path'),
-    pkg = require('./package.json'),
-    env = process.env.NODE_ENV || 'development';
+  pkg = require('./package.json'),
+  env = process.env.NODE_ENV || 'development';
 
-var config = {};
-config.development = {
-    rootPath: __dirname,
-    app: {
-        name: pkg.name,
-        fullName: 'Health Check',
-        version: pkg.version,
-        frontend: '//development.health-check.divshot.io'
-    },
-    mail: {
-        port: 25,
-        host: 'smtp.mailgun.org',
-        auth: {
-            user: '<mailgun user>',
-            pass: '<mailgun password>'
-        }
-    },
-    mongo: {
-        url: 'mongodb://heroku_app35515501:ck2jd4t6mq9dgsapr4saau66r4@ds059651.mongolab.com:59651/heroku_app35515501',
-        options: {
-            debug: false
-        }
-    },
-    casper: {
-        absolutePath: '/usr/bin/casperjs'
-    },
-    session: {
-        secret: '8Kh862PMMabR'
+var config = {
+  rootPath: __dirname,
+  port: process.env.PORT || 5000,
+  sharedKey: '',
+  app: {
+    name: pkg.name,
+    fullName: 'Health Check',
+    version: pkg.version,
+    frontend: '//localhost:5000'
+  },
+  mail: {
+    port: 25,
+    host: '',
+    auth: {
+      user: '',
+      pass: ''
     }
+  },
+  mongo: {
+    url: '<mongodb URL>',
+    options: {
+      debug: true
+    }
+  },
+  casper: {
+    absolutePath: '/usr/local/bin/casperjs'
+  },
+  session: {
+    name: 'hcsid',
+    secret: ''
+  }
 };
 
 module.exports = config;
